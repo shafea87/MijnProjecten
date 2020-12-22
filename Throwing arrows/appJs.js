5e7f45f8c16c4c995d66b5bb9bd4bb57f9fb5afb
shafea1987 <m.s.alsalkini@gmail.com>
2020-12-22 20:37:56 +0100

    a
    aa

--- Throwing arrows/appJs.js
+++ Throwing arrows/appJs.js
@@ -1,39 +1,80 @@
+let yourName = prompt("What is yor name");
+document.getElementById('yourName').innerHTML = "Welcom " + yourName;
+
+
 
 document.getElementById('container').addEventListener('click',onClick);
+let bValue=0,
+    aValue=0,
+    dValue=0,
+    nValue=0,
+    fValue=0,
+    cValue=0,
+    tmp=0;
 function onClick(e){
- 
-  let aValue = document.getElementById('result1').value;
-  let bValue = document.getElementById('result2').value;
-  let cValue = document.getElementById('result3').value;
-  let dValue = document.getElementById('result4').value;
-  let nValue = document.getElementById('result5').value;
-  let fValue = document.getElementById('result6').value;
-
-  
-
+ tmp++;
+ if(tmp<=5){
   if(e.target.className == "inner-5"){
-    document.getElementById('result1').value = 100;
+    aValue+=100;
+     document.getElementById('result1').value=aValue;
+ 
   }else if(e.target.className == "inner-4"){
-    document.getElementById('result2').value = 80 ;
+
+  bValue+=80;
+   document.getElementById('result2').value=bValue;
+ 
     }else if(e.target.className == "inner-3"){
-      document.getElementById('result3').value = 60 ;
+   
+     cValue+=60; 
+     document.getElementById('result3').value=cValue;
     }else if(e.target.className == "inner-2"){
-        document.getElementById('result4').value = 40 ;
+      dValue+=40;
+      document.getElementById('result4').value=dValue;
+
     }else if(e.target.className == "inner-1"){
-      document.getElementById('result5').value = 20 ;
+      fValue+=20;
+      document.getElementById('result5').value=fValue;
+ 
     }else{
-      document.getElementById('result6').value = 0 ;
+      nValue+=10;
+      document.getElementById('result6').value=nValue;
+
     }
 
-   document.getElementById("total").value = 
-   document.getElementById('result1').value+
-    document.getElementById('result2').value+
-   document.getElementById('result3').value+
-    document.getElementById('result4').value+
-  document.getElementById('result5').value+
-  document.getElementById('result6').value;
     
+   document.getElementById("total").value = aValue + bValue + cValue + dValue + nValue + fValue;
+
+  } else{
+    //alert("");
+   let some= confirm("Game finished! start new Game?");
+   if(some){
+console.log(some);
+location.reload();
+return false;
+   } else{
+    // return;
+     location.assign("https://www.google.com");
+   }
+   
+  }
+    
+// switch (key) {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+// }
 }
+
+
+
+
+
+
+
+
 
 
 
