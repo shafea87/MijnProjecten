5bd45cde5117c740824a7e82ca81dea157180330
shafea1987 <m.s.alsalkini@gmail.com>
2021-02-05 21:57:12 +0100

    05-02
    05-02

---
+++ JS/snake/app.js
@@ -0,0 +1,7 @@
+;(function(){
+  // document.getElementById('canvas').getContext('2d').fillRect(20,100,30,30);
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+})();
